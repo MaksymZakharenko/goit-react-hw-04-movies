2179e4c293d6ceb748cf3695d9eef1e03498e00e
MaksymZakharenko <raspik83@gmail.com>
2021-06-18 17:06:54 +0300

    Updates

---
+++ static/js/7.32906e7f.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-04-movies"]=this["webpackJsonpgoit-react-hw-04-movies"]||[]).push([[7],{94:function(t,e,i){"use strict";i.r(e);i(0);var s=i(4);e.default=function(){return Object(s.jsx)("h2",{children:"\u0414\u0430\u043d\u043d\u0430\u044f \u0441\u0442\u0440\u0430\u043d\u0438\u0446\u0430 \u043d\u0435 \u043d\u0430\u0439\u0434\u0435\u043d\u0430!"})}}}]);
+//# sourceMappingURL=7.32906e7f.chunk.js.map
